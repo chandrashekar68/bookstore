922e53f97cce4a9fe36c2b954a8c2b0c0de21069
chandrashekar68 <shekarchandra6868@gmail.com>
2024-03-06 22:28:41 +0530

    added index.js and database connection

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,3 @@
 export const PORT=5555;
+
+export const mongoDBURL = mongodb+srv//shekarchandra6868:NkRPdtQ3I7XX2C33@book-store-mern.rthcxej.mongodb.net/books-collection?retryWrites=true&w=majority&appName=book-store-MERN
--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,25 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT, mongoDBURL} from "./config.js";
+import mongoose from "mongoose";
 const app=express();
 
-app.listen(PORT,()=>{
+app.get('/',(request,response)=>{
+    console.log(request)
+    return response.status(234).send('welcome everyone')
+
+})
+
+
+mongoose
+  .connect(mongoDBURL)
+  .then(()=>{
+    console.log('app connected to database')
+  })
+   .catch(()=>{
+    console.log(error)
+   })
+
+   app.listen(PORT,()=>{
     console.log(`app is listening to port :${PORT}`);
 })
+963.*
